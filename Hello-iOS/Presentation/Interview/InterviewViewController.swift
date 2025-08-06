39ddf991ca194c37e22e6c7daa7fde7123a25c37
Lee-Tae-Yun <cmskzkdl3698@naver.com>
2025-08-06 21:41:00 +0900

    feat: 모의면접 선택 화면 구성

--- Hello-iOS/Presentation/Interview/InterviewViewController.swift
+++ Hello-iOS/Presentation/Interview/InterviewViewController.swift
@@ -12,11 +12,47 @@ import SnapKit
 
 class InterviewViewController: BaseViewController<InterviewReactor> {
 
+  let myStudyInterviewButton = UIButton(type: .system).then {
+    $0.setTitle("내 학습 기반 모의 면접", for: .normal)
+    $0.backgroundColor = .systemBackground
+    $0.layer.cornerRadius = 8
+    $0.tintColor = .label
+    $0.titleLabel?.font = .systemFont(ofSize: 25, weight: .bold)
+    $0.layer.shadowColor = UIColor.black.cgColor
+    $0.layer.shadowOpacity = 0.2
+    $0.layer.shadowOffset = CGSize(width: 0, height: 2)
+    $0.layer.shadowRadius = 4
+  }
+
+  let reviewInterviewButton = UIButton(type: .system).then {
+    $0.setTitle("복습 모의 면접", for: .normal)
+    $0.backgroundColor = .systemBackground
+    $0.layer.cornerRadius = 8
+    $0.tintColor = .label
+    $0.titleLabel?.font = .systemFont(ofSize: 25, weight: .bold)
+    $0.layer.shadowColor = UIColor.black.cgColor
+    $0.layer.shadowOpacity = 0.2
+    $0.layer.shadowOffset = CGSize(width: 0, height: 2)
+    $0.layer.shadowRadius = 4
+  }
+
   override func viewDidLoad() {
     super.viewDidLoad()
-    print("hello world")
-    view.backgroundColor = .blue
+    setUI()
+    setConstraints()
   }
+
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+    myStudyInterviewButton.layer.shadowPath = UIBezierPath(
+      roundedRect: myStudyInterviewButton.bounds,
+      cornerRadius: myStudyInterviewButton.layer.cornerRadius).cgPath
+
+    reviewInterviewButton.layer.shadowPath = UIBezierPath(
+      roundedRect: reviewInterviewButton.bounds,
+      cornerRadius: reviewInterviewButton.layer.cornerRadius).cgPath
+  }
+
   init(reactor: InterviewReactor) {
     super.init(nibName: nil, bundle: nil)
     self.reactor = reactor
@@ -24,5 +60,27 @@ class InterviewViewController: BaseViewController<InterviewReactor> {
 
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
+  }
+
+  // UI 추가
+  private func setUI() {
+    view.addSubview(myStudyInterviewButton)
+    view.addSubview(reviewInterviewButton)
+  }
+
+  //  레이아웃 설정
+  private func setConstraints() {
+    myStudyInterviewButton.snp.makeConstraints {
+      $0.center.equalToSuperview()
+      $0.leading.trailing.equalToSuperview().inset(16)
+      $0.width.equalTo(350)
+    }
+
+    reviewInterviewButton.snp.makeConstraints {
+      $0.top.equalTo(myStudyInterviewButton.snp.bottom).offset(15)
+      $0.centerX.equalToSuperview()
+      $0.leading.trailing.equalToSuperview().inset(16)
+      $0.width.equalTo(350)
+    }
   }
 }
