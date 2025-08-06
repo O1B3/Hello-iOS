ea293692302b883eacb61558ca9109d8a52e4618
Yoon-Mac <sryoowk@gmail.com>
2025-08-06 21:32:10 +0900

    Refactor: 프로퍼티 권장방식으로 변경

--- Hello-iOS/Model/Attendance.swift
+++ Hello-iOS/Model/Attendance.swift
@@ -3,8 +3,8 @@ import Foundation
 
 /// 앱 내에서 출석 UI 표시를 위한 데이터 모델
 struct Attendance: Equatable, Hashable {
-  let date: Date        // 출석한 날짜
-  let isAttendance: Bool // 출석 여부 (true = 출석함)
+  let date: Date          // 출석한 날짜
+  let isAttendance: Bool  // 출석 여부 (true = 출석함)
   
   init(date: Date, isAttendance: Bool = false) {
     self.date = date
