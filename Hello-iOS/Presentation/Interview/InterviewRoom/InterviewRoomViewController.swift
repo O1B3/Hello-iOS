87faf8381c396f360a1319ed3b2206c1f26ac988
Lee-Tae-Yun <cmskzkdl3698@naver.com>
2025-08-07 16:41:13 +0900

    Feat: 화면 UI구성 완료

--- Hello-iOS/Presentation/Interview/InterviewRoom/InterviewRoomViewController.swift
+++ Hello-iOS/Presentation/Interview/InterviewRoom/InterviewRoomViewController.swift
@@ -10,6 +10,7 @@ import RxCocoa
 import Then
 import SnapKit
 import Speech
+import AVFoundation
 
 class InterviewRoomViewController: BaseViewController<InterviewRoomReactor> {
 
@@ -28,18 +29,51 @@ class InterviewRoomViewController: BaseViewController<InterviewRoomReactor> {
     $0.textColor = .label
     $0.numberOfLines = 0
     $0.backgroundColor = .clear
-    $0.font = .systemFont(ofSize: 23, weight: .medium)
+    $0.font = .systemFont(ofSize: 23, weight: .bold)
     $0.text = "Array에 대해서 설명해주세요 Array에 대해서 설명해주세요 Array에 대해서 설명해주세요 Array에 대해서 설명해주세요."
   }
 
   private let interviewerImageView = UIImageView().then {
+    let config = UIImage.SymbolConfiguration(pointSize: 40, weight: .medium)
+    $0.image = UIImage(systemName: "person", withConfiguration: config)
     $0.contentMode = .scaleAspectFit
-    $0.image = UIImage(systemName: "person")?
-      .withConfiguration(UIImage.SymbolConfiguration(pointSize: 40, weight: .medium))
     $0.layer.cornerRadius = 50
     $0.tintColor = .main
     $0.backgroundColor = .card
     $0.clipsToBounds = true
+  }
+
+  private let myAnswerLabel = UILabel().then {
+    $0.textColor = .label
+    $0.numberOfLines = 0
+    $0.backgroundColor = .clear
+    $0.font = .systemFont(ofSize: 23, weight: .medium)
+    $0.text = "Array에 대해서 설명해주세요 Array에 대해서 설명해주세요 Array에 대해서 설명해주세요 Array에 대해서 설명해주세요."
+  }
+
+  private let micStackView = UIStackView().then {
+    $0.axis = .horizontal
+    $0.distribution = .fillEqually
+    $0.spacing = 10
+  }
+
+  private let leftButton = UIButton(type: .system).then {
+    let config = UIImage.SymbolConfiguration(pointSize: 40, weight: .medium)
+    $0.setImage(UIImage(systemName: "chevron.left", withConfiguration: config), for: .normal)
+    $0.tintColor = .main
+  }
+
+  private let microphoneImageView = UIImageView().then {
+    let config = UIImage.SymbolConfiguration(pointSize: 40, weight: .medium)
+    $0.image = UIImage(systemName: "mic", withConfiguration: config)
+    $0.contentMode = .scaleAspectFit
+    $0.tintColor = .main
+  }
+
+  private let rightButton = UIButton(type: .system).then {
+    let config = UIImage.SymbolConfiguration(pointSize: 40, weight: .medium)
+    $0.setImage(UIImage(systemName: "chevron.right", withConfiguration: config), for: .normal)
+    $0.tintColor = .main
   }
 
   override func viewDidLoad() {
@@ -59,9 +93,13 @@ class InterviewRoomViewController: BaseViewController<InterviewRoomReactor> {
 
   // UI 추가
   override func setupUI() {
-    [questionStackView, interviewerImageView].forEach { view.addSubview($0) }
+    [questionStackView, interviewerImageView, myAnswerLabel, micStackView].forEach { view.addSubview($0) }
     questionStackView.addArrangedSubview(verticalBar)
     questionStackView.addArrangedSubview(questionLable)
+    micStackView.addArrangedSubview(leftButton)
+    micStackView.addArrangedSubview(microphoneImageView )
+    micStackView.addArrangedSubview(rightButton)
+
 
   }
 
@@ -80,6 +118,17 @@ class InterviewRoomViewController: BaseViewController<InterviewRoomReactor> {
       $0.top.equalTo(questionStackView.snp.bottom).offset(12)
       $0.centerX.equalToSuperview()
       $0.height.width.equalTo(100)
+    }
+
+    myAnswerLabel.snp.makeConstraints {
+      $0.top.equalTo(interviewerImageView.snp.bottom).offset(12)
+      $0.leading.trailing.equalToSuperview().inset(16)
+    }
+
+    micStackView.snp.makeConstraints {
+      $0.top.equalTo(myAnswerLabel.snp.bottom).offset(12)
+      $0.leading.trailing.equalToSuperview().inset(16)
+      $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(12)
     }
 
   }
