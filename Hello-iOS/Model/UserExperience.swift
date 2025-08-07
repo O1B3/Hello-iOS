5dceab67747ec276b3f18f477a4e9f04d8f5ebff
Yoon-Mac <sryoowk@gmail.com>
2025-08-07 21:03:36 +0900

    Chore: UserExperience  모델 수정

--- Hello-iOS/Model/UserExperience.swift
+++ Hello-iOS/Model/UserExperience.swift
@@ -10,7 +10,7 @@ struct UserExperience {
     switch exp {
     case ..<10: return .egg
     case 10..<20: return .chick
-    case 20..<40: return .hen
+    case 20..<30: return .hen
     default: return .phoenix
     }
   }
@@ -40,11 +40,36 @@ struct UserExperience {
     // 라벨 텍스트
     var labelText: String {
       switch self {
-      case .egg: return "Egg"
-      case .chick: return "Chick"
-      case .hen: return "Hen"
-      case .phoenix: return "Phoenix"
+      case .egg: return "Lv1. Egg"
+      case .chick: return "LV2. Chick"
+      case .hen: return "Lv3. Hen"
+      case .phoenix: return "Lv4. Phoenix"
       }
     }
   }
 }
+
+extension UserExperience {
+  func expProgressAndLabel() -> (Float, String) {
+    let (base, next) = {
+      switch self.level {
+      case .egg: return (0, 10)
+      case .chick: return (10, 20)
+      case .hen: return (20, 30)
+      case .phoenix: return (30, 30) // 만렙 구간도 40~40으로 고정
+      }
+    }()
+    let expToNext = next - base
+    let myExpInStage = exp - base
+    
+    if self.level == .phoenix {
+      // 만렙: 40/40로 고정
+      return (1.0, "(30 / 30)")
+    }
+    let progress = Float(myExpInStage) / Float(expToNext)
+    let label = "(\(myExpInStage) / \(expToNext))"
+    return (progress, label)
+  }
+}
+
+
