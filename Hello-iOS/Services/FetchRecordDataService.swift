0a1d9442cc2f8d3b8b8709a58f5148f8ba11bd16
Yoon-Mac <sryoowk@gmail.com>
2025-08-08 22:46:43 +0900

    Feat: Stub 데이터 생성

--- Hello-iOS/Services/FetchRecordDataService.swift
+++ Hello-iOS/Services/FetchRecordDataService.swift
@@ -21,12 +21,69 @@ struct FetchRecordDataService: FetchRecordDataServiceProtocol {
 
 // Mock Data Testing Code
 struct StubRecordDataService: FetchRecordDataServiceProtocol {
+  
+  let recordA = MockInterviewRecord(
+    id: 0,
+    groupId: "group1",
+    question: "Q",
+    modelAnswer: "A",
+    myAnswer: "MA",
+    isSatisfied: true
+  )
+  
+  let recordB = MockInterviewRecord(
+    id: 1,
+    groupId: "group1",
+    question: "Q",
+    modelAnswer: "A",
+    myAnswer: "MA",
+    isSatisfied: false
+  )
+  
+  let recordC = MockInterviewRecord(
+    id: 2,
+    groupId: "group1",
+    question: "Q",
+    modelAnswer: "A",
+    myAnswer: "MA",
+    isSatisfied: true
+  )
+  
+  let recordD = MockInterviewRecord(
+    id: 0,
+    groupId: "group2",
+    question: "Q",
+    modelAnswer: "A",
+    myAnswer: "MA",
+    isSatisfied: true
+  )
+  
+  let recordE = MockInterviewRecord(
+    id: 1,
+    groupId: "group1",
+    question: "Q",
+    modelAnswer: "A",
+    myAnswer: "MA",
+    isSatisfied: false
+  )
+  
   func fetchinterViewGroups() -> [MockInterviewGroup] {
-    return []
+    return [
+      MockInterviewGroup(
+        id: "group2",
+        date: Date(),
+        records: [recordD, recordE]
+      ),
+      MockInterviewGroup(
+        id: "group1",
+        date: Date().addingTimeInterval(-10000),
+        records: [recordA, recordB, recordC]
+      ),
+    ]
   }
   
   func fetchinterViewRecords(id: Int) -> [MockInterviewRecord] {
-    return []
+    return [recordA, recordB, recordC]
   }
 }
 
