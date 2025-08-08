83d897a39fcc1b27b9ae2cf591c053b2f832c0ca
Yoon-Mac <sryoowk@gmail.com>
2025-08-08 19:19:04 +0900

    Refactor : 모델 세부 리팩토링

--- Hello-iOS/Repositories/RealmMockInterview.swift
+++ Hello-iOS/Repositories/RealmMockInterview.swift
@@ -7,27 +7,30 @@ import RealmSwift
 class RealmMockInterviewGroup: Object {
   @Persisted(primaryKey: true) var id: String             // 그룹 식별자
   @Persisted var date: Date                               // 날짜
-  @Persisted var records: List<RealmMockInterviewRecord>  // 일대다 연결
+  @Persisted var records: List<RealmMockInterviewRecord>  // 레코드
   
   // 도메인 모델 → Realm 모델
   convenience init(from domain: MockInterviewGroup) {
     self.init()
     self.id = domain.id
     self.date = domain.date
-    self.records.append(objectsIn: domain.records.map { RealmMockInterviewRecord(from: $0) })
+    // 생성 시에 groupId 주입
+    let linkedRecords = domain.records.map { RealmMockInterviewRecord(from: $0, groupId: domain.id) }
+    self.records.append(objectsIn: linkedRecords)
   }
   
   // Realm 모델 → 도메인 모델
   func toDomain() -> MockInterviewGroup {
-    MockInterviewGroup(
-      id: id,
-      date: date,
-      records: records.map { $0.toDomain() }
-    )
-  }
+      let filtered = records.filter { $0.groupId == self.id } // groupId 일치 필터링
+      return MockInterviewGroup(
+        id: id,
+        date: date,
+        records: filtered.map { $0.toDomain() }
+      )
+    }
 }
 
-// MARK: 모의면접 질문에 대한 개별 답변 Realm DB
+// MARK: 모의면접 질문에 대한 개별 답변
 class RealmMockInterviewRecord: Object {
   @Persisted var id: Int              // 답변 인덱스
   @Persisted var groupId: String      // 그룹 UUID 문자열
@@ -37,10 +40,10 @@ class RealmMockInterviewRecord: Object {
   @Persisted var isSatisfied: Bool    // 만족 여부
   
   // 도메인 모델 → Realm 모델
-  convenience init(from domain: MockInterviewRecord) {
+  convenience init(from domain: MockInterviewRecord, groupId: String) {
     self.init()
     self.id = domain.id
-    self.groupId = domain.groupId
+    self.groupId = groupId
     self.question = domain.question
     self.modelAnswer = domain.modelAnswer
     self.myAnswer = domain.myAnswer
