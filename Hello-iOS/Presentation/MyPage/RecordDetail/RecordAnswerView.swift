1bae9a386872f1903b0d2e42a0d4a9afc3d61b2e
Yoon-Mac <sryoowk@gmail.com>
2025-08-11 21:48:35 +0900

    Rename: 변수명 리네이밍

--- Hello-iOS/Presentation/MyPage/RecordDetail/RecordAnswerView.swift
+++ Hello-iOS/Presentation/MyPage/RecordDetail/RecordAnswerView.swift
@@ -2,34 +2,34 @@
 import UIKit
 
 final class RecordAnswerView: UIView {
-  private let title = UILabel()
+  private let question = UILabel()
   private let model = UILabel()
   private let mine  = UILabel()
-  private let stack = UIStackView()
+  private let stackView = UIStackView()
   
   init(item: MockInterviewRecord) {
     super.init(frame: .zero)
     layer.cornerRadius = 12
     clipsToBounds = true
     
-    [title, model, mine].forEach {
+    [question, model, mine].forEach {
       $0.numberOfLines = 0
       $0.lineBreakMode = .byWordWrapping
     }
-    title.font = .systemFont(ofSize: 15, weight: .semibold)
+    question.font = .systemFont(ofSize: 15, weight: .semibold)
     
-    stack.axis = .vertical
-    stack.spacing = 8
-    stack.isLayoutMarginsRelativeArrangement = true
-    stack.layoutMargins = .init(top: 12, left: 12, bottom: 12, right: 12)
+    stackView.axis = .vertical
+    stackView.spacing = 8
+    stackView.isLayoutMarginsRelativeArrangement = true
+    stackView.layoutMargins = .init(top: 12, left: 12, bottom: 12, right: 12)
     
-    addSubview(stack)
-    stack.snp.makeConstraints { $0.edges.equalToSuperview() }
-    [title, model, mine].forEach(stack.addArrangedSubview)
+    addSubview(stackView)
+    stackView.snp.makeConstraints { $0.edges.equalToSuperview() }
+    [question, model, mine].forEach(stackView.addArrangedSubview)
 
-    title.text = "Q. " + item.question
-    model.text = item.modelAnswer
-    mine.text  = item.myAnswer
+    question.text = "질문 : \n " + item.question
+    model.text = "모법 답안 : \n" + item.modelAnswer
+    mine.text  = "내 답안 : \n" + item.myAnswer
     backgroundColor = (item.isSatisfied ? UIColor.correct : UIColor.wrong )
       .withAlphaComponent(0.20)
     
