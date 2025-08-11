8d673ca57b7bf3ee7e731266bb50763b355a840b
juseojo <j86371224@gmail.com>
2025-08-11 17:51:23 +0900

    Feat: 단어장 비즈니스 로직 구현

--- Hello-iOS/Presentation/WordBook/WordBookViewController.swift
+++ Hello-iOS/Presentation/WordBook/WordBookViewController.swift
@@ -29,43 +29,6 @@ class WordBookViewController: BaseViewController<WordBookReactor> {
 
   override func viewDidLoad() {
     self.navigationItem.title = "단어장"
-
-    let mockConcepts = MockConcept(
-      id: 1,
-      categoryId: 1,
-      concept: "MVVM",
-      explain: "대충 MVVM에 대한 설명",
-      latestUpdate: Date(),
-      isMemory: true
-    )
-
-    let mockConcepts2 = MockConcept(
-      id: 2,
-      categoryId: 1,
-      concept: "MVVM",
-      explain: "대충 MVVM에 대한 설명",
-      latestUpdate: Date(),
-      isMemory: false
-    )
-
-    let mockData1 = MockWordBook(
-      category: "디자인 패턴",
-      id: 1,
-      concepts: [mockConcepts, mockConcepts2]
-    )
-    let mockData2 = MockWordBook(category: "디자인 패턴", id: 2, concepts: [mockConcepts])
-    let mockData3 = MockWordBook(category: "디자인 패턴", id: 3, concepts: [mockConcepts])
-    let mockData4 = MockWordBook(category: "디자인 패턴", id: 4, concepts: [mockConcepts])
-    let mockData5 = MockWordBook(category: "디자인 패턴", id: 5, concepts: [mockConcepts])
-    let mockData6 = MockWordBook(category: "디자인 패턴", id: 6, concepts: [mockConcepts])
-
-    wordBookView.dataApply(data: [mockData1,
-                     mockData2,
-                     mockData3,
-                     mockData4,
-                     mockData5,
-                     mockData6
-                    ])
   }
 
   override func bind(reactor: WordBookReactor) {
@@ -77,5 +40,20 @@ class WordBookViewController: BaseViewController<WordBookReactor> {
         self?.navigationController?.pushViewController(vc, animated: true)
       })
       .disposed(by: disposeBag)
+
+    self.rx
+      .viewDidLoad
+      .subscribe(onNext: { _ in reactor.action.onNext(.fetchWordBook) })
+      .disposed(by: disposeBag)
+    
+    reactor
+        .state
+        .bind { [weak dataSource = wordBookView.dataSource] item in
+          var snapshot = NSDiffableDataSourceSnapshot<Int, DomainCategories>()
+            snapshot.appendSections([0])
+          snapshot.appendItems(item.wordBooks, toSection: 0)
+          dataSource?.apply(snapshot, animatingDifferences: true)
+        }
+        .disposed(by: disposeBag)
   }
 }
